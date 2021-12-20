5499647753b4d240b7052f2b4982ee81fe32da62
Sakib Hadziavdic <sakib@sake.ba>
2021-12-20 20:27:30 +0100

    Add readme for semigroup

--- fp/src/main/scala/fp/semigroup/Semigroup.scala
+++ fp/src/main/scala/fp/semigroup/Semigroup.scala
@@ -1,4 +1,4 @@
-package fp
+package fp.semigroup
 
 trait Semigroup[T] {
   extension (x: T) def <>(y: T): T
@@ -8,6 +8,10 @@ object Semigroup {
   given Semigroup[Int] with
     extension (x: Int) def <>(y: Int): Int = x + y
 
+  given Semigroup[String] with
+
+    extension (x: String) def <>(y: String): String = x + y
+
   given [T]: Semigroup[List[T]] with
     extension (x: List[T]) def <>(y: List[T]): List[T] = x ++ y
 }
@@ -15,5 +19,6 @@ object Semigroup {
 @main def semigroupMain: Unit =
   import Semigroup.given
   println(1 <> 2)
+  println("abc" <> "def")
   println(List(1, 2, 3) <> List(4, 5, 6))
 //println(true <> false)
